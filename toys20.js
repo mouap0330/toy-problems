bc0c5e441e086ab699d2b6f62ef87bd89aef8a72
Pao Moua <mouap0330@gmail.com>
2018-10-29 17:32:19 -0700

    isLessThan30

---
+++ toys20.js
@@ -0,0 +1,12 @@
+/*
+Write a function called "isLessThan30".
+Given a number, "isLessThan30" returns whether the given number is less than 30.
+var output = isLessThan30(9);
+console.log(output); // --> true
+*/
+
+var output = isLessThan30(31);
+function isLessThan30(num) {
+  return num < 30 ? true : false;
+}
+console.log(output); // --> true
