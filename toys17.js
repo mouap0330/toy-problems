d2282a90782f3d00600ab14b95e7d19b4c406163
Pao Moua <mouap0330@gmail.com>
2018-10-29 17:04:45 -0700

    getLengthOfWord

---
+++ toys17.js
@@ -0,0 +1,12 @@
+/*
+Write a function called "getLengthOfWord".
+Given a word, "getLengthOfWord" returns the length of the given word.
+var output = getLengthOfWord('some');
+console.log(output); // --> 4
+*/
+
+var output = getLengthOfWord('asdfghjkl');
+function getLengthOfWord(word) {
+  return word.length
+}
+console.log(output); // --> 4
