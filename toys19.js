754d45e461a14b50ab937f2e69b2e02c744b8db2
Pao Moua <mouap0330@gmail.com>
2018-10-29 17:30:39 -0700

    isGreaterThanTen

---
+++ toys19.js
@@ -0,0 +1,13 @@
+
+/*
+Write a function called "isGreaterThanTen".
+Given a number, "isGreaterThanTen" returns whether it is greater than 10.
+var output = isGreaterThanTen(11);
+console.log(output); // --> true
+*/
+
+var output = isGreaterThanTen(10);
+function isGreaterThanTen(num) {
+  return num > 10 ? true : false;
+}
+console.log(output); // --> true
