82562f7495b121bb0f60967023aa87c83370e672
Pao Moua <mouap0330@gmail.com>
2018-10-30 19:34:44 -0700

    computePower

---
+++ toys56.js
@@ -0,0 +1,10 @@
+/*
+Write a function called "computePower".
+Given a number and an exponent, "computePower" returns the given number, raised to the given exponent.
+*/
+
+function computePower(num, exponent) {
+  return Math.pow(2,3);
+}
+var output = computePower(2, 3);
+console.log(output); // --> 8
