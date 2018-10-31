59bc5f306d4235a9118dbfbe607824d5e1e321b2
Pao Moua <mouap0330@gmail.com>
2018-10-30 18:32:57 -0700

    isOldEnoughToVote

---
+++ toys42.js
@@ -0,0 +1,16 @@
+/*
+Write a function called "isPersonOldEnoughToVote".
+Given a "person" object, that contains an "age" property, "isPersonOldEnoughToVote" returns whether the given person is old enough to vote.
+Notes:
+* The legal voting age in the United States is 18.
+*/
+var obj = {
+  age: 19
+};
+
+var output = isPersonOldEnoughToVote(obj);
+
+function isPersonOldEnoughToVote(person) {
+  return (person.age >= 18) ? true : false;
+}
+console.log(output); // --> true
