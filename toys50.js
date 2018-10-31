745c9bcfe4a3152637a7add5681e6a1766272c1e
Pao Moua <mouap0330@gmail.com>
2018-10-30 18:56:09 -0700

    computeAreaRect

---
+++ toys50.js
@@ -0,0 +1,10 @@
+/*
+Write a function called "computeAreaOfARectangle".
+Given the length and width of a rectangle, "computeAreaOfARectangle" returns its area.
+*/
+
+var output = computeAreaOfARectangle(4, 8);
+function computeAreaOfARectangle(length, width) {
+ return length * width;
+}
+console.log(output); // --> 32
