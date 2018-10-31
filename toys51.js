b3435c107a63e1366cfdb0eff8b8802ebb5a66ea
Pao Moua <mouap0330@gmail.com>
2018-10-30 18:57:33 -0700

    computePerimOfRect

---
+++ toys51.js
@@ -0,0 +1,10 @@
+/*
+Write a function called "computePerimeterOfARectangle".
+Given a length and a width describing a rectangle, "computePerimeterOfARectangle" returns its perimter.
+*/
+
+var output = computePerimeterOfARectangle(5, 2);
+function computePerimeterOfARectangle(length, width) {
+  return (length * 2) + (width * 2);
+}
+console.log(output); // --> 14
