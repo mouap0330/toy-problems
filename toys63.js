d770404c8dbfd6798fc2b8967c1199eff0dc68fa
Pao Moua <mouap0330@gmail.com>
2018-11-01 18:50:22 -0700

    getAllElementsButFirst

---
+++ toys63.js
@@ -0,0 +1,13 @@
+/*
+Write a function called "getAllElementsButFirst".
+
+Given an array, "getAllElementsButFirst" returns an array with all the elements but the first.
+
+*/
+
+function getAllElementsButFirst(array) {
+  return newArray = array.slice(1);
+}
+var input = [1, 2, 3, 4];
+var output = getAllElementsButFirst(input);
+console.log(output); // --> [2, 3, 4]
