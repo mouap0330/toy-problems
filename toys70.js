1b64a655205baf31e405a9d08a5c92fb67ba4a81
Pao Moua <mouap0330@gmail.com>
2018-11-02 23:43:08 -0700

    getAllWords

---
+++ toys70.js
@@ -0,0 +1,19 @@
+/*
+Write a function called "getAllWords".
+
+Given a sentence, "getAllWords" returns an array containing every word in the sentence.
+
+Notes:
+* If given an empty string, it should return an empty array.
+*/
+
+function getAllWords(str) {
+  if (str === '') {
+    return [];
+  } else {
+    return str.split(' ');
+  }
+}
+
+var output = getAllWords('Radagast the Brown');
+console.log(output); // --> ['Radagast', 'the', 'Brown']
