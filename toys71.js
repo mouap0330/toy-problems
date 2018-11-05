063868ff253ba592bbcf5fb3a617e213026fc49a
Pao Moua <mouap0330@gmail.com>
2018-11-05 08:08:34 -0800

    countWords

---
+++ toys71.js
@@ -0,0 +1,28 @@
+/*
+Write a function called "countWords".
+
+Given a string, "countWords" returns an object where each key is a word in the given string, with its value being how many times that word appeared in th given  string.
+
+Notes:
+* If given an empty string, it should return an empty object.
+*/
+
+function countWords(str) {
+  var obj = {};
+  var newStr = str.split(' ');
+
+  if (newStr === '') {
+    return {};
+  }
+  for (var i = 0; i < newStr.length; i++) {
+    if(newStr[i] in obj) {
+      obj[newStr[i]]++;
+    } else {
+      obj[newStr[i]] = 1;
+    }
+  }
+  return obj;
+}
+
+var output = countWords('ask a bunch get a bunch');
+console.log(output); // --> {ask: 1, a: 2, bunch: 2, get: 1}
